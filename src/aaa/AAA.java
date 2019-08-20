b9d79792b4205bf527ce0a58ac3e434aad5215d7
liuwenyu <18231193861@163.com>
2019-08-20 17:31:51 +0800

    test

---
+++ src/aaa/AAA.java
@@ -0,0 +1,25 @@
+package aaa;
+
+import java.util.Random;
+
+public class AAA {
+	static final int PRICE_TABLE_MAX = 3;
+	static final int PRICE_TABLE_TIME = 300;
+	static void Demo(){
+		String result="";
+		int i;
+	    Random random = new Random();
+	    for(i = 0;i < PRICE_TABLE_MAX;i++){
+	    	result+=random.nextInt(10);
+	        System.out.println(result);
+	     }	       
+	}
+	public static void main(String[] args){
+		int i;
+        for( i = 0;i <PRICE_TABLE_TIME;i++){
+           Demo();
+        }
+       
+		}
+
+}
