fec51e7481d75528d4a9c2413ca92785c684beae
liuwenyu <18231193861@163.com>
2019-08-23 16:54:28 +0800

    我这次是commit and push

--- src/aaa/AAA.java
+++ src/aaa/AAA.java
@@ -4,6 +4,7 @@ import java.util.Random;
 // 我正在开发，这个模块car_shop，现在我开发完了，合回到feature/car_shop
 // 这个功能开发测试完了，准备发布，基于develop创建。
 // 我今天开发了一些功能
+// 我要commitandpush了，
 public class AAA {
 	static final int PRICE_TABLE_MAX = 3;
 	static final int PRICE_TABLE_TIME = 300;
